0792f3ff636035e634a6e3ebee156e52f29ed130
Valeriya K <saleria-v@mail.ru>
2024-02-10 02:14:18 +0300

    feat createMovie/movie/controllers

---
+++ controllers/movie.js
@@ -0,0 +1,42 @@
+const Movie = require('../models/movie');
+const { OK, CREATED_OK } = require('../responses/responses');
+const BadRequest = require('../errors/bad-request-err');
+
+module.exports.createMovie = (req, res, next) => {
+  const {
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    thumbnail,
+    movieId,
+    nameRU,
+    nameEN,
+  } = req.body;
+  const owner = req.user._id;
+  Movie.create({
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    thumbnail,
+    movieId,
+    nameRU,
+    nameEN,
+    owner,
+  })
+    .then((movie) => res.status(CREATED_OK).send({ data: movie }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new BadRequest('Переданы некорректные данные'));
+        return;
+      }
+      next(err);
+    });
+};
