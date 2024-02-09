c21d8d1c00c75b65df45d3d3d7c8ee6485bb8a6d
Valeriya K <saleria-v@mail.ru>
2024-02-10 02:40:41 +0300

    feat deleteMovie/controllers

--- controllers/movie.js
+++ controllers/movie.js
@@ -1,6 +1,8 @@
 const Movie = require('../models/movie');
 const { OK, CREATED_OK } = require('../responses/responses');
 const BadRequest = require('../errors/bad-request-err');
+const ForbiddenError = require('../errors/forbidden-err');
+const NotFoundError = require('../errors/not-found-err');
 
 module.exports.createMovie = (req, res, next) => {
   const {
@@ -34,6 +36,29 @@ module.exports.createMovie = (req, res, next) => {
     .then((movie) => res.status(CREATED_OK).send({ data: movie }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
+        next(new BadRequest('Переданы некорректные данные'));
+        return;
+      }
+      next(err);
+    });
+};
+
+module.exports.deleteMovie = (req, res, next) => {
+  const { movieId } = req.params;
+  Movie.findById(movieId)
+    .then((movie) => {
+      if (movie === null) {
+        next(new NotFoundError('Фильм не найден'));
+      }
+      if (movie.owner.toString() !== req.user._id) {
+        return next(new ForbiddenError('Вы не можете удалять фильмы других пользователей'));
+      } return movie.deleteOne()
+        .then(() => {
+          res.status(OK).send({ message: 'Фильм успешно удалён' });
+        });
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
         next(new BadRequest('Переданы некорректные данные'));
         return;
       }
